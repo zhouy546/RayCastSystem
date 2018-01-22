6df5840f4c97a760dc9254ff834250857f01253d
zhouy546 <zhouyunqi546@gmail.com>
2018-01-22 14:08:14 +0800

    Initial commit

---
+++ Assets/Script/ClientScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientScript : MonoBehaviour {
+
+    // Use this for initialization
+    public void SubScribe()
+    {
+        RayCast.OnClickEvent += this.OnClick;
+    }
+
+    public void UnSubscribe()
+    {
+        this.GetComponent<Renderer>().material.color = Color.white;
+        RayCast.OnClickEvent -= this.OnClick;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void OnClick() {
+        this.GetComponent<Renderer>().material.color = Color.red;
+        Debug.Log(this.name+" " + "is triggered");
+    }
+}
---
+++ Assets/Script/RayCast.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+    public delegate void  Onclick();
+    public static event Onclick OnClickEvent;
+    public Transform RayCastHolder;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    // Update is called once per frame
+    void Update () {
+        OnClick();
+    }
+
+    void OnClick() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    if (hit.collider.tag == "InteractiveObj")
+                    {
+                        if (RayCastHolder == null)
+                        {
+                            RayCastHolder = hit.collider.transform;
+                            SubScribeAndDeSub(true);
+                            Debug.Log("Hit different object, from null to object");
+                        }
+                        else {
+                            if (RayCastHolder != hit.collider.transform)
+                            {
+                                Debug.Log("Hit different object, unsubscribe the perivous one");
+                                SubScribeAndDeSub(false);
+
+                                RayCastHolder = hit.collider.transform;
+
+                                SubScribeAndDeSub(true);
+                                Debug.Log("Hit different object, subscribe the current one");
+                            }
+                            else
+                            {
+                                RayCastHolder = hit.collider.transform;
+                                Debug.Log("Hit same object");
+                            }
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                UnSlect();
+            }
+            if (OnClickEvent!=null)
+            OnClickEvent();
+        }
+    }
+
+    void SubScribeAndDeSub( bool i) {
+        if (RayCastHolder != null) {
+            if (RayCastHolder.GetComponent<ClientScript>() != null)
+            {
+                ClientScript client = RayCastHolder.GetComponent<ClientScript>();
+                if (!i)
+                {
+                    client.UnSubscribe();
+                }
+                else
+                {
+                    client.SubScribe();
+                }
+            }
+        }
+    }
+
+
+    public void UnSlect() {
+        Debug.Log("HIT NOTHING Current client DeSubscribe !");
+        SubScribeAndDeSub(false);
+        RayCastHolder = null;
+    }
+}
+
